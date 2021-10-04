7a7181173a6dc1ca3793b211268a58c69bf8f86e
Priscila <priscila.mizuiky@gmail.com>
2021-10-04 10:58:44 -0300

    Trying to add animator logic + game sounds

--- LudumDareProject/Assets/Scripts/Gift.cs
+++ LudumDareProject/Assets/Scripts/Gift.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Gift : MonoBehaviour
 {
-    public void getGift()
+    public void hideGift()
     {
         this.gameObject.SetActive(false);
         //player adiciona pontos ao score
         //som de pegou um objeto, pode ser um plim
     }
 
+    public void showGift()
+    {
+        this.gameObject.SetActive(true);
+    }
 
 }
